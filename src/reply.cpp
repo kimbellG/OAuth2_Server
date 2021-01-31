668c182c5c770db61d8c5f204073e7922e78156b
Артём <filippenkov95@gmail.com>
2021-01-31 19:32:00 +0300

    Подправил вывод

--- src/reply.cpp
+++ src/reply.cpp
@@ -13,11 +13,11 @@ namespace http
 	Answer::Answer(char *input, const char *server_name) 
 		: request(input), __server_name(server_name), __code(OK)
 	{
-		const int time_strlen = 32;
+		const int time_strlen = 34;
 		char time_str[time_strlen];
 		std::time_t t_value = std::time(NULL);
 		std::tm *t_struct = std::localtime(&t_value); //Sun, 31 Jan 2021 15:55:34 GMT
-		std::strftime(time_str, time_strlen, "%a, %e %b %G %H:%M:%S %Z", t_struct); 
+		std::strftime(time_str, time_strlen, "%a, %e %b %G %H:%M:%S GMT%Z", t_struct); 
 #ifdef DEBUG_REPLY
 		std::cout <<  start_mes::debug_mes << "Time: " << time_str << std::endl;
 #endif //DEBUG_REPLY
@@ -28,13 +28,13 @@ namespace http
 		__add_http_header("Date", time_str);
 		// __add_file_to_answer();
 #ifdef DEBUG_REPLY
-		std::cout << "\tHEADERS" << std::endl;
+		std::cout << start_mes::debug_mes << "\tHEADERS" << std::endl;
 
 		for (std::vector<header>::iterator start = __http_headers.begin();
 				start != __http_headers.end(); start++)
 		{
-			std::cout << "Name: " << start->name << std::endl;
-			std::cout << "Value: " << start->value << std::endl;
+			std::cout << start_mes::debug_mes << "Name: " << start->name << std::endl;
+			std::cout << start_mes::debug_mes << "Value: " << start->value << std::endl;
 		}
 #endif // DEBUG_REPLY	
 	}
