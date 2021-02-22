da60f73f52b21f90611b4c580012fb946d9029f7
Артём <filippenkov95@gmail.com>
2021-02-22 09:10:40 +0300

    Начал добавлять функционал классу error.

--- src/errors.cpp
+++ src/errors.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include "../include/reply.hpp"
+
 namespace http
 {
 	namespace error
@@ -10,9 +12,23 @@ namespace http
 				const std::string &filename) 
 			: __code(code), __err_message(err_message), __filename(filename)
 		{
+			__http_headers = std::make_shared<std::vector<header>>();
 
+			add_header("Server", server_name);
+			add_header("Time", Answer::get_current_time());
 		}
 
+		void Error::add_header(const std::string &name, const std::string &value)
+		{
+			__http_headers->push_back({name, value});
+		}
 
+		std::unique_ptr<std::string> Error::get_answer() const
+		{
+		}
+
+		Error::~Error()
+		{
+		}
 	}
 }
